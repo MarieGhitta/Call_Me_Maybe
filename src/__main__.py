c0d32826531e72f1f559694740aa4d09330beb04
mghitta <mghitta@student.19.be>
2026-06-12 15:58:52 +0200

    lancer llm

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,22 @@
 from .models_loader import load_prompts, load_functions
+from llm_sdk.llm_sdk import Small_LLM_Model
 
 
 def main():
     try:
         loaded_prompt = load_prompts("data/input/function_calling_tests.json")
-        print("\n---------prompts--------")
-        print(loaded_prompt)
-        print()
         loaded_functions = load_functions("data/input/functions_definition.json")
-        print("\n---------functions--------")
-        print(loaded_functions)
-        print()
+        model = Small_LLM_Model()
+        for prompt in loaded_prompt:
+            tokens = model.encode(prompt.prompt)
+            print(tokens)
+            fn_name = model.decode(tokens)
+            print(fn_name)
+        for fn in loaded_functions:
+            tokens = model.encode(fn.name)
+            print(tokens)
+            fn_name = model.decode(tokens)
+            print(fn_name)
     except Exception as e:
         print(e)
 
