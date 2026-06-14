75d6dad3ef7d22a91d4bc2b194eaa8a97a69f982
mghitta <mghitta@student.19.be>
2026-06-14 22:04:05 +0200

    before going school 14/06

--- src/__main__.py
+++ src/__main__.py
@@ -7,16 +7,31 @@ def main():
         loaded_prompt = load_prompts("data/input/function_calling_tests.json")
         loaded_functions = load_functions("data/input/functions_definition.json")
         model = Small_LLM_Model()
-        for prompt in loaded_prompt:
-            tokens = model.encode(prompt.prompt)
-            print(tokens)
-            fn_name = model.decode(tokens)
-            print(fn_name)
-        for fn in loaded_functions:
-            tokens = model.encode(fn.name)
-            print(tokens)
-            fn_name = model.decode(tokens)
-            print(fn_name)
+        # for prompt in loaded_prompt:
+        #     tokens = model.encode(prompt.prompt)
+        #     print(tokens)
+        #     fn_name = model.decode(tokens)
+        #     print(fn_name)
+        # get_logits_from_input_ids()
+        # for fn in loaded_functions:
+        #     tokens = model.encode(fn.name)
+        #     print(tokens)
+        #     fn_name = model.decode(tokens)
+        #     print(fn_name)
+        i = 0
+        test = "Il était une fois"
+        tokens = model.encode(test)
+        while i < 20:
+            tokens = model.encode(test)
+            logits = model.get_logits_from_input_ids(tokens.tolist()[0])
+            valeur_max = max(logits)
+            indice = logits.index(valeur_max)
+            premslogits = model.decode([indice])
+            # print(f"{test} {premslogits}")
+            test = test + premslogits
+            i += 1
+        print(test)
+        
     except Exception as e:
         print(e)
 
