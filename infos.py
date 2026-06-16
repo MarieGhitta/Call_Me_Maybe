073ddc378439de7f0519ef0c8392dd96b3a29232
mghitta <mghitta@student.19.be>
2026-06-16 15:44:41 +0200

    beginning constraints

--- infos.py
+++ infos.py
@@ -56,3 +56,31 @@
             else:
                 raise GenerationError("Max token limit exceeded")
         
+
+
+                # for prompt in loaded_prompt:
+        #     tokens = model.encode(prompt.prompt)
+        #     print(tokens)
+        #     fn_name = model.decode(tokens)
+        #     print(fn_name)
+        # get_logits_from_input_ids()
+        # for fn in loaded_functions:
+        #     tokens = model.encode(fn.name)
+        #     print(tokens)
+        #     fn_name = model.decode(tokens)
+        #     print(fn_name)
+        
+        #i = 0
+        #test = "Choose the most fitting function with this instruction"
+        #tokens = model.encode(test)
+        #while i < 20:
+        #    tokens = model.encode(test)
+        #    logits = model.get_logits_from_input_ids(tokens.tolist()[0])
+        #    valeur_max = max(logits)
+        #    indice = logits.index(valeur_max)
+        #    premslogits = model.decode([indice])
+        #    # print(f"{test} {premslogits}")
+        #    test = test + premslogits
+        #    i += 1
+        #print(test)
+
--- src/__main__.py
+++ src/__main__.py
@@ -1,41 +1,38 @@
 from .models_loader import load_prompts, load_functions
-import numpy as np
-# from llm_sdk.llm_sdk import Small_LLM_Model
+# import numpy as np
+from llm_sdk.llm_sdk import Small_LLM_Model
+
+
+def function_tokenisation(loaded_functions: list, model: Small_LLM_Model):
+    tokenized_fn = {}
+    for fn in loaded_functions:
+        tokenized_fn[fn.name] = model.encode(fn.name).tolist()[0]
+    return tokenized_fn
+
+
+def compatible_functions(tokens_generated: list, fonctions_tokenised: dict):
+    comp_fn = []
+    for name, value in fonctions_tokenised.items():
+        if tokens_generated == value[:len(tokens_generated)]:
+            comp_fn.append(name)
+    return comp_fn
+
+
+def allowed_next_tokens(tokens_generated: list, fonctions_tokenised: dict):
+    comp_fn = compatible_functions(tokens_generated, fonctions_tokenised)
+    allowed_tok = set()
+    for cf in comp_fn:
+        allowed_tok.add(fonctions_tokenised[cf][len(tokens_generated)])
+    return allowed_tok
 
 
 def main():
     try:
         loaded_prompt = load_prompts("data/input/function_calling_tests.json")
         loaded_functions = load_functions("data/input/functions_definition.json")
-        # model = Small_LLM_Model()
-        
-        # for prompt in loaded_prompt:
-        #     tokens = model.encode(prompt.prompt)
-        #     print(tokens)
-        #     fn_name = model.decode(tokens)
-        #     print(fn_name)
-        # get_logits_from_input_ids()
-        # for fn in loaded_functions:
-        #     tokens = model.encode(fn.name)
-        #     print(tokens)
-        #     fn_name = model.decode(tokens)
-        #     print(fn_name)
-        
-        #i = 0
-        #test = "Choose the most fitting function with this instruction"
-        #tokens = model.encode(test)
-        #while i < 20:
-        #    tokens = model.encode(test)
-        #    logits = model.get_logits_from_input_ids(tokens.tolist()[0])
-        #    valeur_max = max(logits)
-        #    indice = logits.index(valeur_max)
-        #    premslogits = model.decode([indice])
-        #    # print(f"{test} {premslogits}")
-        #    test = test + premslogits
-        #    i += 1
-        #print(test)
-
-
+        model = Small_LLM_Model()
+        tokenised_functions = function_tokenisation(loaded_functions, model)
+        print(compatible_functions([8822, 2891], tokenised_functions))
     except Exception as e:
         print(e)
 
