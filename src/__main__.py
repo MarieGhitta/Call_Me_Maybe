a6ce6a10b626a6644b92764a5dccf65d15e05033
mghitta <mghitta@student.19.be>
2026-06-17 23:21:40 +0200

    beginning boucle

--- src/__main__.py
+++ src/__main__.py
@@ -18,13 +18,31 @@ def compatible_functions(tokens_generated: list, fonctions_tokenised: dict):
     return comp_fn
 
 
-def allowed_next_tokens(tokens_generated: list, fonctions_tokenised: dict):
-    comp_fn = compatible_functions(tokens_generated, fonctions_tokenised)
+def allowed_next_tokens(tokens_generated: list, functions_tokenised: dict):
+    comp_fn = compatible_functions(tokens_generated, functions_tokenised)
     allowed_tok = set()
     for cf in comp_fn:
-        allowed_tok.add(fonctions_tokenised[cf][len(tokens_generated)])
+        allowed_tok.add(functions_tokenised[cf][len(tokens_generated)])
     return allowed_tok
 
+
+def select_fn(model: Small_LLM_Model, 
+              user_prompt: str, 
+              loaded_functions: list, 
+              tokenized_functions):
+    prompt = "Choose the most appropriate function for the user request.\n"
+    for fn in loaded_functions:
+        prompt += f"\nFunction: {fn.name}"
+        prompt += f"\nDescription: {fn.description}\n"
+    prompt += f"\nUser Request: {user_prompt}\n"
+    prompt += "\nSelected function:"
+    tokenised_prompt = model.encode(prompt).tolist()[0]
+    logits_fn = model.get_logits_from_input_ids(tokenised_prompt)
+    generate_tokens = []
+    allowed = allowed_next_tokens(generate_tokens, tokenized_functions)
+    for token in allowed:
+        print(token, logits_fn[token])
+    return allowed
 
 def main():
     try:
@@ -32,7 +50,9 @@ def main():
         loaded_functions = load_functions("data/input/functions_definition.json")
         model = Small_LLM_Model()
         tokenised_functions = function_tokenisation(loaded_functions, model)
-        print(compatible_functions([8822, 2891], tokenised_functions))
+        compatible_fn = compatible_functions([8822, 2891], tokenised_functions)
+        print(select_fn(model, "What is the sum of 2 and 3?", loaded_functions, tokenised_functions))
+
     except Exception as e:
         print(e)
 
